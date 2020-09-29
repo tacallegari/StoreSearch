58ee423f86ad6d83298fa4f763c9c22183d108f4
tacallegari <62042181+tacallegari@users.noreply.github.com>
2020-09-28 23:01:50 -0400

    Add a search bar and table view. The search puts fake results in the table for now

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Tahlia Callegari on 9/28/20.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
